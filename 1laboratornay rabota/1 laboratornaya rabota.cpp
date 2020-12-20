8fd4080bc84a1fbff0de9c0442f034abc6e72d0e
zhadyra <zhadok.s2002@gmail.com>
2020-12-20 20:02:32 +0300

    classwork+homework

---
+++ 1laboratornay rabota/1 laboratornaya rabota.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	char simvol;
+	cout << "Hello World" << endl;
+	setlocale(LC_ALL, "Russian");
+	cout << "Введите любой символ:\n";
+	cin >> simvol;
+	system("pause");
+	return 0;
+}
